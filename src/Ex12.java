06e6686bb5ea1c2b08063f0559c1c4ede234c5c0
CauaCristian <cauainocencio14@hotmail.com>
2023-02-18 19:34:52 -0300

    lista-1

---
+++ src/Ex12.java
@@ -0,0 +1,6 @@
+public class Ex12{
+    public static void main(String[] args) {
+        System.out.println("Aprendendo Algoritmo !!!");
+        System.out.println("Com Anita e Guto");
+    }
+}
---
+++ src/Ex16.java
@@ -0,0 +1,7 @@
+public class Ex16 {
+    public static void main(String[] args) {
+        int x;
+        x = 10;
+        System.out.println(x);
+    }
+}
